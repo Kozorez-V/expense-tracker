286010b8c9143a6050391e2193bd126bc117e5f6
Kozorez-V <kozorezviktoria@gmail.com>
2022-06-24 16:53:11 +0400

    creating database

--- expense_tracker/catalog/models.py
+++ expense_tracker/catalog/models.py
@@ -1,2 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Profile(models.Model):
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE,
+                                primary_key=True)
+    limit = models.IntegerField(blank=True, null=True)
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    date = models.DateField(blank=False, null=False)
+    name = models.CharField(max_length=50, blank=False)
+    amount = models.FloatField(blank=False, null=False)
+
+class Category(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    expense = models.ForeignKey(Expense, on_delete=models.CASCADE, null=False)
+    name = models.CharField(max_length=30, blank=False)
