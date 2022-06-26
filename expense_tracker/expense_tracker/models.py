46a77f9c0a25a2e991463b75bcb0991a51bb9f35
Kozorez-V <kozorezviktoria@gmail.com>
2022-06-26 12:40:23 +0400

    profile and category are combined by many-to-many

--- expense_tracker/expense_tracker/models.py
+++ expense_tracker/expense_tracker/models.py
@@ -2,13 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
 
-class Profile(models.Model):
-    user = models.OneToOneField(User,
-                                on_delete=models.CASCADE,
-                                primary_key=True)
-    limit_flag = models.BooleanField(blank=False, default=False)
-    limit = models.PositiveIntegerField(blank=True, null=True)
-
 class Expense(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     date = models.DateField(blank=False, null=False)
@@ -19,3 +12,11 @@ class Category(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     expense = models.ForeignKey(Expense, on_delete=models.CASCADE, null=False)
     name = models.CharField(max_length=30, blank=False, unique=True)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE,
+                                primary_key=True)
+    categories = models.ManyToManyField(Category)
+    limit_flag = models.BooleanField(blank=False, default=False)
+    limit = models.PositiveIntegerField(blank=True, null=True)
