f69e0451d73d95f8c443d32354ba42af54b5e635
Kozorez-V <kozorezviktoria@gmail.com>
2022-07-06 13:11:57 +0400

    admin edit

--- expense_tracker/expense_tracker/admin.py
+++ expense_tracker/expense_tracker/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import Profile, Expense, Category
 
-admin.site.register(Profile)
-admin.site.register(Expense)
-admin.site.register(Category)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'limit_flag', 'limit')
+    list_display_links = ['user']
+    search_fields = ['user']
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'category', 'date', 'name', 'amount')
+    list_display_links = ('user', 'category', 'name')
+    search_fields = ('user', 'date', 'category', 'amount')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'name')
+    list_display_links = ('pk', 'user', 'name')
+    search_fields = ('user', 'name')
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Expense, ExpenseAdmin)
+admin.site.register(Category, CategoryAdmin)
