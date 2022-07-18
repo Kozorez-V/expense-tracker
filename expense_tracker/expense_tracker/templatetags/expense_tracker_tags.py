bb32931f909e3a8048ec3ada0b5124ed32555a0d
Kozorez-V <kozorezviktoria@gmail.com>
2022-07-18 15:23:25 +0400

    pagination

--- expense_tracker/expense_tracker/templatetags/expense_tracker_tags.py
+++ expense_tracker/expense_tracker/templatetags/expense_tracker_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from expense_tracker.models import *
 
+from django.core.paginator import Paginator
+
 register = template.Library()
 
 
@@ -9,4 +11,9 @@ def expense_history(context):
     request = context['request']
     expenses = Expense.objects.filter(user=request.user).order_by('-date')
 
-    return {'expenses': expenses}
+    paginator = Paginator(expenses, 10)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return {'page_obj': page_obj}
