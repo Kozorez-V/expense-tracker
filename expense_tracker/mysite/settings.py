33cbe9653c27537d4abbc43b0da1528cb5a72f2a
Kozorez-V <kozorezviktoria@gmail.com>
2022-08-13 15:54:12 +0400

    install bootstrap

--- expense_tracker/mysite/settings.py
+++ expense_tracker/mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'expense_tracker.apps.CatalogConfig',
     'debug_toolbar',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -123,7 +124,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
