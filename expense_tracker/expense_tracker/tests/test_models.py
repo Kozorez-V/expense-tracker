06c15a2cce0cf4f73af9a20c28e374c5e6ec128f
Kozorez-V <kozorezviktoria@gmail.com>
2022-09-11 17:54:05 +0400

    creating CategoryModelTest

--- expense_tracker/expense_tracker/tests/test_models.py
+++ expense_tracker/expense_tracker/tests/test_models.py
@@ -8,11 +8,24 @@ class UserModelTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        User.objects.create(username='crown', password='1234pass', email='crown@gmail.com')
+        User.objects.create(username='user', password='1234pass', email='crown@gmail.com')
 
     def test_creating_profile(self):
         user = User.objects.get(pk=1)
-        self.assertEquals(User.objects.get(profile__user=1), user)
+        self.assertEqual(User.objects.get(profile__user=1), user)
 
     def test_user_has_default_categories(self):
-        self.assertEquals(Category.objects.filter(user=1).count(), 9)
+        self.assertEqual(Category.objects.filter(user=1).count(), 9)
+
+
+class CategoryModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create(username='user', password='1234pass', email='crown@gmail.com')
+        Category.objects.create(user=user, name='Одежда')
+
+    def test_string_method(self):
+        category = Category.objects.get(pk=10)
+        expected_string = 'Одежда'
+        self.assertEquals(str(category), expected_string)
